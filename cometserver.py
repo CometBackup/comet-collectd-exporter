6eb307d685a5474b8a3a2b325ac554ad002befed
Mason <mason@cometbackup.com>
2021-07-05 13:40:18 +1200

    normalize spaces to tabs

--- cometserver.py
+++ cometserver.py
@@ -8,44 +8,44 @@ import urllib.request
 import urllib.parse
 
 try:
-    COLLECTD_HOSTNAME = os.environ["COLLECTD_HOSTNAME"]
-    COLLECTD_INTERVAL = int(float(os.environ["COLLECTD_INTERVAL"]))
-    COMETSERVER_URL   = sys.argv[1]
-    COMETSERVER_USER  = sys.argv[2]
-    COMETSERVER_PASS  = sys.argv[3]
-    NAMESPACE         = sys.argv[4]
+	COLLECTD_HOSTNAME = os.environ["COLLECTD_HOSTNAME"]
+	COLLECTD_INTERVAL = int(float(os.environ["COLLECTD_INTERVAL"]))
+	COMETSERVER_URL   = sys.argv[1]
+	COMETSERVER_USER  = sys.argv[2]
+	COMETSERVER_PASS  = sys.argv[3]
+	NAMESPACE         = sys.argv[4]
 
 except Exception:
 	print("Usage:\nCOLLECTD_HOSTNAME=localhost COLLECTD_INTERVAL=60 ./cometserver.py COMETSERVER_URL USER PASS NAMESPACE")
 	sys.exit(1)
 
 class CometServer(object):
-    def __init__(self, url, adminuser, adminpass):
-        self.url = url
-        self.adminuser = adminuser
-        self.adminpass = adminpass
+	def __init__(self, url, adminuser, adminpass):
+		self.url = url
+		self.adminuser = adminuser
+		self.adminpass = adminpass
 
-    def AdminListUsers(self):
-        """List all usernames on the Comet Server"""
-        return self._request("api/v1/admin/list-users", {})
+	def AdminListUsers(self):
+		"""List all usernames on the Comet Server"""
+		return self._request("api/v1/admin/list-users", {})
 
-    def _request(self, endpoint, extraparams):
-        """Make API request to Comet Server and parse response JSON"""
-        apiRequest = urllib.request.Request(
-            url = self.url + endpoint,
-            data = urllib.parse.urlencode({
-                "Username": self.adminuser,
-                "AuthType": "Password",
-                "Password": self.adminpass,
-                **extraparams
-            }).encode('utf-8')
-        )
+	def _request(self, endpoint, extraparams):
+		"""Make API request to Comet Server and parse response JSON"""
+		apiRequest = urllib.request.Request(
+			url = self.url + endpoint,
+			data = urllib.parse.urlencode({
+				"Username": self.adminuser,
+				"AuthType": "Password",
+				"Password": self.adminpass,
+				**extraparams
+			}).encode('utf-8')
+		)
 
-        ret = None
-        with urllib.request.urlopen(apiRequest) as apiResponse:
-            ret = json.loads( apiResponse.read() )
+		ret = None
+		with urllib.request.urlopen(apiRequest) as apiResponse:
+			ret = json.loads( apiResponse.read() )
 
-        return ret
+		return ret
 
 def get_metrics(srv: CometServer):
 	userList = srv.AdminListUsers()
